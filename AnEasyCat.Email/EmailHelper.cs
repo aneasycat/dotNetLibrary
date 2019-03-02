9cbe46c40243d548f45db2934698269255ccb8dd
i2807 <i2807@DESKTOP-KCU7DCJ>
2019-03-02 21:42:27 +0800

    增加 发送邮件辅助类

---
+++ AnEasyCat.Email/EmailHelper.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace AnEasyCat.Email
+{
+    public class EmailHelper
+    {
+        private string Sender = "";
+        private SmtpClient smtp;
+        public string Error = "";
+        public EmailHelper(Models.Config config)
+        {
+            Sender = config.Sender;
+            smtp = new SmtpClient(config.Host, config.Port)
+            {
+                Credentials = new NetworkCredential(config.Sender, config.Password)
+            };
+        }
+        /// <summary>
+        /// 发送邮件
+        /// </summary>
+        /// <param name="body"></param>
+        /// <returns></returns>
+        public bool Send(Models.Body body)
+        {
+            body.Sender = Sender;
+            try
+            {
+                smtp.Send(body.MailMessage);
+                return true;
+            }
+            catch(SmtpException ex)
+            {
+                Error = ex.Message.ToString();
+                return false;
+            }
+        }
+        public bool Send(MailMessage message)
+        {
+            try
+            {
+                smtp.Send(message);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public string Send(params Models.Body[] bodys)
+        {
+            string reStr = "";
+            foreach(var body in bodys)
+            {
+                reStr += Send(body);
+            }
+            return reStr;
+        }
+        /// <summary>
+        /// 异步发送邮件
+        /// </summary>
+        /// <param name="body"></param>
+        /// <returns></returns>
+        public bool SendAsync(Models.Body body)
+        {
+            body.Sender = Sender;
+            try
+            {
+                smtp.SendAsync(body.MailMessage,null);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public string SendAsync(params Models.Body[] bodys)
+        {
+            string reStr = "";
+            foreach (var body in bodys)
+            {
+                reStr += SendAsync(body);
+            }
+            return reStr;
+        }
+    }
+}
---
+++ AnEasyCat.Email/Models.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Net.Mail;
+
+namespace AnEasyCat.Email
+{
+    public class Models
+    {
+        public class Config
+        {
+            /// <summary>
+            /// smtp服务器地址
+            /// </summary>
+            public string Host { get; set; }
+            /// <summary>
+            /// smtp服务器端口
+            /// </summary>
+            public int Port { get; set; } = 25;
+            /// <summary>
+            /// 发件人邮箱账号
+            /// </summary>
+            public string Sender { get; set; }
+            /// <summary>
+            /// 发件人邮箱密码
+            /// </summary>
+            public string Password { get; set; }
+        }
+        public class Body
+        {
+            /// <summary>
+            /// 发件人显示名
+            /// </summary>
+            public string SenderName { get; set; }
+            /// <summary>
+            /// 收件人
+            /// </summary>
+            public string[] Recipients { get; set; }
+            /// <summary>
+            /// 邮件标题
+            /// </summary>
+            public string Title { get; set; }
+            /// <summary>
+            /// 邮件优先级
+            /// </summary>
+            public MailPriority Priority { get; set; } = MailPriority.Normal;
+            /// <summary>
+            /// 邮件内容
+            /// </summary>
+            public string Content { get; set; }
+            /// <summary>
+            /// 邮件内容是否是html格式
+            /// </summary>
+            public bool IsHtml { get; set; } = false;
+            /// <summary>
+            /// 邮件附件
+            /// </summary>
+            public IList<Attachment> Attachments { get; set; }
+            /// <summary>
+            /// 邮件编码
+            /// </summary>
+            public Encoding Encoding { get; set; } = Encoding.UTF8;
+            private MailMessage message = new MailMessage();
+            public MailMessage MailMessage
+            {
+                get
+                {
+                    message.Subject = Title;
+                    message.Priority= Priority;
+                    message.Body = Content;
+                    message.IsBodyHtml = IsHtml;
+                    message.From = new MailAddress(Sender, SenderName, Encoding);
+                    message.BodyEncoding = message.SubjectEncoding = Encoding;
+
+                    if (Recipients != null && Recipients.Length > 0)
+                        foreach (var recipient in Recipients)
+                        {
+                            message.To.Add(recipient);
+                        }
+                    if (Attachments != null && Attachments.Count > 0)
+                        foreach (var attachment in Attachments)
+                        {
+                            message.Attachments.Add(attachment);
+                        }
+                    return message;
+                }
+            }
+            /// <summary>
+            /// 发件人地址-不赋值
+            /// </summary>
+            public string Sender { get; set; }
+        }
+    }
+}
