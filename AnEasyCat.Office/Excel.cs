890461169f00bdc8cf6da7b3b8120b22a2b8418f
i2807 <i2807@DESKTOP-KCU7DCJ>
2019-03-11 15:14:51 +0800

    创建OFFICE操作类

---
+++ AnEasyCat.Office/Excel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Core;
+using Microsoft.Office.Interop.Excel;
+
+namespace AnEasyCat.Office
+{
+    public class Excel
+    {
+        string _file="";
+        public Excel(string File)
+        {
+            _file = File;
+        }
+    }
+}
